9255041eb5ff461a0d0d463e1065ccf95c519f10
SangjinH <54434944+SangjinH@users.noreply.github.com>
2022-06-29 00:13:20 +0900

    commit project structure

---
+++ Effective_Java/effectivejava/src/main/java/Builder/Exercise.java
@@ -0,0 +1,14 @@
+package Builder;
+
+public class Exercise {
+
+    NyPizza nyPizza = new NyPizza.Builder(NyPizza.Size.SMALL)
+                                .addTopping(Pizza.Topping.SAUSAGE)
+                                .addTopping(Pizza.Topping.ONION)
+                                .build();
+
+    Calzone calzone = new Calzone.Builder()
+                                .addTopping(Pizza.Topping.HAM)
+                                .sauceInside()
+                                .build();
+}
